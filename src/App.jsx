ffebabbe11ed6f1c9d0f52f0f6370783c965542b
Girish <girishpawar055@gmail.com>
2023-04-04 16:16:25 +0530

    renderd application images added

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,35 @@ import Navbar from './Components/Navbar'
 import Sidebar from './Components/Sidebar'
 import Feed from './Components/Feed'
 import Rightbar from './Components/Rightbar'
-import { Box, Container, Grid, Stack } from '@mui/material'
+import { Grid, Stack } from '@mui/material'
 import Add from './Components/Add'
 import { useState } from 'react'
-import { createTheme, ThemeProvider} from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 const App = () => {
 
     const [mode, setMode] = useState("dark")
 
-    const darkTheme = createTheme ({
-        palette : {
-            mode : mode
+    const darkTheme = createTheme({
+        palette: {
+            mode: mode
         }
     })
 
     return (
-        <ThemeProvider theme = {darkTheme}>
+        <ThemeProvider theme={darkTheme}>
 
-        <Grid bgcolor={"background.default"} color = {"text.primary"} // for dark the white theme 
-        >  
-            <Navbar/>
-            <Stack direction={'row'} spacing = {2} justifyContent = "space-between">
-                <Sidebar setMode = {setMode} mode = {mode}/>
-                <Feed/>
-                <Rightbar/>
-            </Stack>
-            <Add/>
-        </Grid>
+            <Grid bgcolor={"background.default"} color={"text.primary"} // for dark the white theme 
+            >
+                <Navbar />
+                <Stack direction={'row'} spacing={2} justifyContent="space-between">
+                    <Sidebar setMode={setMode} mode={mode} />
+                    <Feed />
+                    <Rightbar />
+                </Stack>
+                <Add />
+            </Grid>
         </ThemeProvider>
     )
 }
